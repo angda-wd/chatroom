24f7f3a7f55a2bb8efc1d417289570669af6a865
angda_wd <2908049163@qq.com>
2019-08-22 19:25:39 +0800

    json

--- src/main/java/com/angda/util/CommUtil.java
+++ src/main/java/com/angda/util/CommUtil.java
@@ -1,4 +1,7 @@
 package com.angda.util;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -7,9 +10,12 @@ import java.util.Properties;
  * 封装所有的公共操作，包括加载配置文件，json操作
  */
 public class CommUtil {
+    private static  final Gson GSON=new GsonBuilder().create();
+
     public static  Properties loadProperties(String fileName){
         Properties properties=new Properties();
-        InputStream in=CommUtil.class.getClassLoader().getResourceAsStream(fileName);
+        InputStream in=CommUtil.class.getClassLoader()
+                .getResourceAsStream(fileName);
         try {
             properties.load(in);
         } catch (IOException e) {
@@ -18,6 +24,12 @@ public class CommUtil {
             return null;
         }
         return properties;
+    }
+    public static String object2Json(Object object){
+        return GSON.toJson(object);
+    }
+    public static Object json2Object(String jsonStr,Class object){
+        return GSON.fromJson(jsonStr,object);
     }
 
 }
