0fb92b4a2e7f74e4ac127d9c1ebea0beb64d1fbd
angda_wd <2908049163@qq.com>
2019-08-22 19:24:59 +0800

    Data

--- src/main/java/com/angda/client/entity/User.java
+++ src/main/java/com/angda/client/entity/User.java
@@ -1,39 +1,11 @@
 package com.angda.client.entity;
+
+import lombok.Data;
+
+@Data
 public class User {
     private Integer id;
     private String username;
     private String password;
     private String breif;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getBreif() {
-        return breif;
-    }
-
-    public void setBreif(String breif) {
-        this.breif = breif;
-    }
 }
