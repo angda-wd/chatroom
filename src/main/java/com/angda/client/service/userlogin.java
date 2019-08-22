ec6a271a2d32abe0ae2a4826194c1f0bee9a6cb5
angda_wd <2908049163@qq.com>
2019-08-22 19:24:20 +0800

    登录

--- src/main/java/com/angda/client/service/userlogin.java
+++ src/main/java/com/angda/client/service/userlogin.java
@@ -2,10 +2,16 @@ package com.angda.client.service;
 
 import com.angda.client.dao.AccountDao;
 import com.angda.client.entity.User;
+import com.angda.util.CommUtil;
+import com.angda.vo.MessageVO;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+import java.util.Set;
 
 public class userlogin {
     private JPanel loginPanel;
@@ -18,6 +24,12 @@ public class userlogin {
 
     private AccountDao accountDao=new AccountDao();
     public userlogin() {
+        JFrame frame = new JFrame("用户登录");
+        frame.setContentPane(loginPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
         //点击注册按钮 跳转注册
         regBtn.addActionListener(new ActionListener() {
             @Override
@@ -33,8 +45,36 @@ public class userlogin {
                 String password=String.valueOf(passwordText.getPassword());
                 User user=accountDao.userLogin(username,password);
                 if(user!=null){
+                    //登陆成功
                     JOptionPane.showMessageDialog(null,
                             "登陆成功","提示信息",JOptionPane.INFORMATION_MESSAGE);
+                    //与服务器建立连接，将用户名注册到服务器缓存
+                    Connect2Server connect2Server=new Connect2Server();
+                    MessageVO messageVO=new MessageVO();
+                    messageVO.setType(1);
+                    messageVO.setContent(username);
+                    String msgJson= CommUtil.object2Json(messageVO);
+                    try {
+                        //发送信息
+                        PrintStream out=new PrintStream(connect2Server.getOut(),
+                                true,"UTF-8");
+                        out.println(msgJson);
+                        //读取服务端发回的响应，加载用户列表
+                        Scanner in=new Scanner(connect2Server.getIn());
+                        if(in.hasNextLine()){
+                            String jsonStr=in.nextLine();
+                            MessageVO msgFromServer= (MessageVO) CommUtil.json2Object(jsonStr,MessageVO.class);
+                            Set<String> names= (Set<String>) CommUtil.json2Object(msgFromServer.getContent(),Set.class);
+                            System.out.println("在线好友为:"+names);
+                            //加载好友列表，登录页面不可见
+                            frame.setVisible(false);
+                            //跳转到好友列表需要传递用户名和与服务器建立的连接
+                            new FriendList(username,connect2Server,names);
+                        }
+                    } catch (UnsupportedEncodingException ex) {
+                        ex.printStackTrace();
+                    }
+
                 }else {
                     JOptionPane.showMessageDialog(null,
                             "登陆失败","失败信息",JOptionPane.ERROR_MESSAGE);
@@ -44,11 +84,6 @@ public class userlogin {
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("用户登录");
-        frame.setContentPane(new userlogin().loginPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+      new userlogin();
     }
 }
